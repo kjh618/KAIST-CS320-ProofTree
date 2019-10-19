d71735e66904e0583d3da71b7671322e4dd0f9bb
Jihoon Kim <kjh618@kaist.ac.kr>
2019-10-19 16:52:33 +0900

    Change symbol for empty map

--- proof-tree/src/main/scala/prooftree/Env.scala
+++ proof-tree/src/main/scala/prooftree/Env.scala
@@ -3,10 +3,24 @@ package prooftree
 object Env {
   type Env = Map[String, Value]
 
-  def envToString(env: Env): String = env.map({ case (n, v) => s"$n ↦ $v" }).mkString("[", ", ", "]")
+  def envToString(env: Env): String = {
+    if (env.isEmpty)
+      "∅"
+    else
+      env.map({ case (n, v) => s"$n ↦ $v" }).mkString("[", ", ", "]")
+  }
 
-  def envDomainToString(env: Env): String = env.map({ case (n, _) => s"$n" }).mkString("[", ", ", "]")
+  def envDomainToString(env: Env): String = {
+    if (env.isEmpty)
+      "∅"
+    else
+      env.map({ case (n, _) => s"$n" }).mkString("[", ", ", "]")
+  }
 
-  def envToReducedString(env: Env, numNotReduce: Int): String =
-    env.map({ case (n, v) => s"$n ↦ ${v.toReducedString(numNotReduce)}" }).mkString("[", ", ", "]")
+  def envToReducedString(env: Env, numNotReduce: Int): String = {
+    if (env.isEmpty)
+      "∅"
+    else
+      env.map({ case (n, v) => s"$n ↦ ${v.toReducedString(numNotReduce)}" }).mkString("[", ", ", "]")
+  }
 }
